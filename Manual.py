ddff8d799f03d2dfd8ff7c1f75be7c032b56bfe5
Martin Laspiur <martin@laspiur.com.ar>
2023-05-19 10:11:12 -0300

    Manual.py

---
+++ Manual.py
@@ -0,0 +1,15 @@
+class Git:
+    def Versiones():
+        pass
+        #git version
+        #git init: dentro de la carpeta donde trabajas para iniciar la relacion
+        #git status -s: es la que te indica como esta cada archivo
+        #git add <archivo> te sube al primer nivel el archivo para hacer el seguimiento.
+        #git commit -m "guia de lo que giarde" : paso todos los archivos temporales a una carpeta definitiva.
+        #git add . : es para todoslos archivos que contiene
+        #git log --oneline: es el dir del dos de los commit realizado
+        #git reset --hard <numero de codigo> trabajo con la version que indico
+        #git push: una vez creada el repossitorio emn la nube asi lo subes
+        #git pull: trae las actualizaciones realizadas en el Remoto y las fuciona con el de aca
+        # GitHub ''> Inesita63
+
